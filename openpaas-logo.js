28a7a5f6a3e4c6b7caabc670b434371ef2295d3b
Michael Bailly <mbailly@linagora.com>
2014-09-26 16:18:52 +0200

    Also box-size the sub-divs

--- openpaas-logo.js
+++ openpaas-logo.js
@@ -30,8 +30,9 @@
   angular.module('openpaas-logo', [])
   .service('op:injectCss', function() {
     var rules = [
-      '.openpaas-logo{width:11em;height:11em;position:relative;display:inline-block;-moz-box-sizing:border-box;box-sizing:border-box;' +
-      '-o-box-sizing:border-box;-ms-box-sizing:border-box;-webkit-box-sizing:border-box; }',
+      '.openpaas-logo{width:11em;height:11em;position:relative;display:inline-block;}',
+      '.openpaas-logo,.openpaas-logo>div{-moz-box-sizing:content-box;box-sizing:content-box;' +
+      '-o-box-sizing:content-box;-ms-box-sizing:content-box;-webkit-box-sizing:content-box;}',
       '.openpaas-logo>.circle{position: absolute;border-radius: 40em;}',
       '.openpaas-logo>.blue-circle{height:3em;width:3em;border:0.9em solid rgb(89,122,183);top:1em;left:0.6em;',
       '.openpaas-logo>.yellow-circle{height:3.4em;width:3.4em;border:1em solid rgb(241,206,40);top:3.7em;left:5.2em;',
